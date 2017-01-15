dfb1099808fc6b8b76666446ec83b6f491532503
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2017-01-15 14:27:15 -0200

    initial implementation

--- src/main/java/org/techstuff/MakingAnagrams.java
+++ src/main/java/org/techstuff/MakingAnagrams.java
@@ -1,10 +1,33 @@
 package org.techstuff;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by andersonkmi on 1/15/2017.
  */
 public class MakingAnagrams {
     public int numberNeeded(String first, String second) {
-        return 0;
+        if(first == null && second != null) {
+            return second.length();
+        } else if(first != null && second == null) {
+            return first.length();
+        }
+
+        List<Character> secondStr = new ArrayList<Character>();
+        for(int pos = 0; pos < second.length(); pos++) {
+            secondStr.add(second.charAt(pos));
+        }
+
+        int pairsFound = 0;
+        for(int index = 0; index < first.length(); index++) {
+            char item = first.charAt(index);
+            if(secondStr.contains(item)) {
+                pairsFound++;
+                secondStr.remove(item);
+                break;
+            }
+        }
+        return (first.length() - pairsFound) + (second.length() - pairsFound);
     }
 }
