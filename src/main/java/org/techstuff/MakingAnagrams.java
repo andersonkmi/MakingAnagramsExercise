e1f77d02c3e3acb9146f3dc97490cdd45ffbe1ee
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2017-01-15 12:54:04 -0200

    Initial commit

---
+++ src/main/java/org/techstuff/MakingAnagrams.java
@@ -0,0 +1,10 @@
+package org.techstuff;
+
+/**
+ * Created by andersonkmi on 1/15/2017.
+ */
+public class MakingAnagrams {
+    public int numberNeeded(String first, String second) {
+        return 0;
+    }
+}
---
+++ src/test/java/org/techstuff/MakingAnagramsTest.java
@@ -0,0 +1,13 @@
+package org.techstuff;
+
+import org.junit.Test;
+
+/**
+ * Created by andersonkmi on 1/15/2017.
+ */
+public class MakingAnagramsTest {
+    @Test
+    public void testAnagrams() {
+
+    }
+}
